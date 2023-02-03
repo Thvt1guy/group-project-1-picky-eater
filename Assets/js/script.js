3b36b68be9bd417408b38c5455b7db4429223a02
[Derrick Alcius] <[derrickalcius@gmail.com]>
2023-02-02 20:38:18 -0500

    added assets folder and index.html

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6f39b688f3msh710dd9da1b7341ep1a85aajsn085d4449e741',
+		'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
+	}
+};
+
+fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/90210/0', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
