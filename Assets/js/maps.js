20d9d51aaa4f222f312496381b1f5df3abd9731b
[Derrick Alcius] <[derrickalcius@gmail.com]>
2023-02-09 20:53:39 -0500

    Fixed map and map marker

--- Assets/js/maps.js
+++ Assets/js/maps.js
@@ -1,28 +1,12 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW50cG9udCIsImEiOiJjbGR1eDRjeHkwMXpiM3BwZ2hlYjcwOHpnIn0.M4sVNfG-Gzfup4Gpspmprw';
 
 
-
-function successLocation(position) {
-    console.log(position)
-    setupMap([position.coords.longitude, position.coords.latitude])
-}
-
-function errorLocation() { 
-    setupMap([])
-}
-
-
-function setupMap(center) { 
-
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: center,
-    zoom: 12,
-})
-}
-
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-    enableHighAccuracy: true
-})
-
+/**
+ * adds map based on given longitude and latitude
+ * @param {float} longitude 
+ * @param {float} latitude 
+ */
+function addingMap(longitude, latitude){
+    return `<img alt='static Mapbox map of the San Francisco bay area' src='https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s+555555(${longitude},${latitude})/${longitude},${latitude},15,0.00,0.00/600x600@2x?access_token=pk.eyJ1IjoidGh2dDFndXkiLCJhIjoiY2xkdXVlOXh3MDljNTNvcGNzbm4xM2o3biJ9.DDrsJrhNjKnPlLlVAiQSBA' >`
+    }
+     
+    
